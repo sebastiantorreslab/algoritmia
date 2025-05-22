df90113e598d16b219eb471dd42765f03cdf2c22
sebastiantorreslab <str9371@gmail.com>
2025-05-21 22:27:26 -0500

    o(N)

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -7,7 +8,18 @@ public class Main {
         ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
         Integer number = sumArr(numbers);
         System.out.println(number);
+
+        Integer max = max(numbers);
+        System.out.println("this is max");
+        System.out.println(max);
+
+        Integer min = min(numbers);
+        System.out.println("this is min");
+        System.out.println( min);
+
     }
+
+    //Complejidad O(n)
 
     public static Integer sumArr(ArrayList<Integer> nums){
         Integer num = 0;
@@ -17,4 +29,39 @@ public class Main {
         }
         return num;
     }
+
+    //Complejidad O(n)
+
+    public static Integer max(ArrayList<Integer> nums) {
+        Integer max = nums.getFirst();
+        for(int i = 0 ; i < nums.size(); i++){
+            if(nums.get(i) > max){
+                max = nums.get(i);
+            }
+        }
+
+        return max;
+    }
+
+
+    //Complejidad O(n)
+    public  static Integer min(ArrayList<Integer> nums) {
+        Integer min = nums.getFirst();
+        for(int i = 0 ; i < nums.size(); i ++) {
+            if (nums.get(i) < min) {
+                min = nums.get(i);
+            }
+        }
+        return min;
+    }
+
+    //Complejidad O(n^2)
+
+
+
+
+
+
+
+
 }
